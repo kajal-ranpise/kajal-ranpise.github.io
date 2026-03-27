a18601000389d6597b0499da35e80279941bfbde
Kajal Ranpise <48114252+kajal-ranpise@users.noreply.github.com>
2026-03-27 15:57:17 +0530

    Update app.js

--- client/src/app.js
+++ client/src/app.js
@@ -31,7 +31,7 @@ function App() {
 
         <main className="flex-grow-1">
       <Routes>
-        <Route path="/My-Profile-Kajal" element={<Profile />} />
+        <Route path="/" element={<Profile />} />
         <Route path="/about" element={<About />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/services" element={<Services />} />
